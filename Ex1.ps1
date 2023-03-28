292bab37f8a915092a64214e3b842fc0282d28fa
Marek Ostrowski <ostrowskimarek7@outlook.com>
2023-03-28 15:18:57 +0200

    Excercise no.1

---
+++ Ex1.ps1
@@ -0,0 +1,16 @@
+﻿#Ex1 - accumulate#
+
+#Create array
+$array = @()
+do {
+ $input = (Read-Host "Please enter elem of array")
+ if ($input -ne '') {$array += $input}
+}
+until ($input -eq '')
+
+#operation square
+foreach ($elem in $array){
+
+$x = [Math]::Pow($array[$elem-1],2)
+Write-Output $x
+}
